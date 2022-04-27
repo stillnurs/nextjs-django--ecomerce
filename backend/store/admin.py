e9c1c8866c38e862131064f4d75dad881693bd25
stillnurs <67423522+stillnurs@users.noreply.github.com>
2022-04-28 03:40:44 +0400

    added models

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-# Register your models here.
+from .models import (
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecificationValue,
+    ProductType,
+)
+
+admin.site.register(Category, MPTTModelAdmin)
