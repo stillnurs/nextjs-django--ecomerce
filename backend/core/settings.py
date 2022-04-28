0fae4aa2942cd85d1236c9ce26aa6141d0dd62ec
stillnurs <67423522+stillnurs@users.noreply.github.com>
2022-04-28 16:10:01 +0400

    added rest_framework, added view

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "store",
     # packages
     "mptt",
+    "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,14 @@ MEDIA_ROOT = BASE_DIR / "media/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Rest Framework
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
--- backend/store/admin.py
+++ backend/store/admin.py
@@ -11,3 +11,25 @@ from .models import (
 )
 
 admin.site.register(Category, MPTTModelAdmin)
+
+
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [ProductSpecificationInline]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductSpecificationValueInline(admin.TabularInline):
+    model = ProductSpecificationValue
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductSpecificationValueInline, ProductImageInline]
